e5c773839cdf067e874585185a96074bb8e22ad8
YOCKOW <YOCKOW@users.noreply.github.com>
2017-11-08 10:39:13 +0900

    Implement `CGIResponder.respond()` partially.

--- Tests/CGIResponderTests/CGIResponderTests.swift
+++ Tests/CGIResponderTests/CGIResponderTests.swift
@@ -1,5 +1,13 @@
+/***************************************************************************************************
+ CGIResponderTests.swift
+   © 2017 YOCKOW.
+     Licensed under MIT License.
+     See "LICENSE.txt" for more information.
+ **************************************************************************************************/
+
 import XCTest
 @testable import CGIResponder
+import Foundation
 
 class CGIResponderTests: XCTestCase {
   func testContentType() {
@@ -13,8 +21,24 @@ class CGIResponderTests: XCTestCase {
     XCTAssertEqual(responder.stringEncoding, .utf8)
   }
   
+  func testOutput() {
+    var output = Data()
+    var responder = CGIResponder()
+    responder.status = .ok
+    responder.contentType = MIMEType(pathExtension:.txt, parameters:["charset":"UTF-8"])!
+    responder.content = .string("Hello, World!\n", encoding:.utf8)
+    
+    let expected = "Status: \(HTTPStatusCode.ok.rawValue) \(HTTPStatusCode.ok.reasonPhrase)\r\n" +
+      "\(HTTPHeaderFieldName.contentType.rawValue): \(MIMEType(pathExtension:.txt, parameters:["charset":"UTF-8"])!.description)\r\n" +
+      "\r\n" +
+      "Hello, World!\n"
+    
+    XCTAssertNoThrow(try responder.respond(to:&output))
+    XCTAssertEqual(String(data:output, encoding:.utf8), expected)
+  }
   
   static var allTests: [(String, (CGIResponderTests) -> () -> Void)] = [
     ("testContentType", testContentType),
+    ("testOutput", testOutput),
   ]
 }
