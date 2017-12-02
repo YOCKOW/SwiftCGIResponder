38610e8d30b55145ad7559fcad724ecc392055a0
YOCKOW <YOCKOW@users.noreply.github.com>
2017-12-02 11:40:15 +0900

    Update version to "0.1.0-dev20171202.next"

--- build-install.rb
+++ build-install.rb
@@ -8,7 +8,7 @@ build-install.rb
 =end
 
 ModuleName = 'CGIResponder'
-ModuleVersion = "0.1.0-dev20171202"
+ModuleVersion = "0.1.0-dev20171202.next"
 
 # Requirements #####################################################################################
 require 'fileutils'
