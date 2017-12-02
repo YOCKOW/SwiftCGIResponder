8b12d578d11f1a8f8fa674ae1b1c23bc5b278e21
YOCKOW <YOCKOW@users.noreply.github.com>
2017-12-02 10:54:44 +0900

    Add `Server`, that represents for the server.

---
+++ Sources/CGIResponder/Server.swift
@@ -0,0 +1,37 @@
+/***************************************************************************************************
+ Server.swift
+   © 2017 YOCKOW.
+     Licensed under MIT License.
+     See "LICENSE.txt" for more information.
+ **************************************************************************************************/
+
+import Foundation
+
+/**
+ 
+ # Server
+ Represents for the server.
+ 
+ */
+public class Server {
+  public static let server = Server()
+  private init() {}
+}
+
+extension Server {
+  /// Returns server's hostname
+  public var hostname: Hostname? {
+    return EnvironmentVariables.default[.serverName] as! Hostname?
+  }
+  
+  /// Returns server's IP address.
+  public var ipAddress: IPAddress? {
+    return EnvironmentVariables.default[.serverAddress] as! IPAddress?
+  }
+  
+  /// Returns string of server software.
+  public var software: String? {
+    return EnvironmentVariables.default[.serverSoftware] as! String?
+  }
+}
+
