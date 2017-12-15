370c43a873064a02043b16b8a6efa94cd16295e5
YOCKOW <YOCKOW@users.noreply.github.com>
2017-12-15 13:10:25 +0900

    Prepare the next development release. (Ver. 0.1.0-dev20171215)

--- build-install.rb
+++ build-install.rb
@@ -8,7 +8,7 @@ build-install.rb
 =end
 
 ModuleName = 'CGIResponder'
-ModuleVersion = "0.1.0-dev20171212.next"
+ModuleVersion = "0.1.0-dev20171215"
 
 # Requirements #####################################################################################
 require 'fileutils'
