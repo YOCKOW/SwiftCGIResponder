eb908410bc6b5f35e532558ec0503e85283c5771
YOCKOW <YOCKOW@users.noreply.github.com>
2018-09-23 10:38:59 +0900

    [HTTP] Add protocol `HeaderFieldValueConvertible`.

---
+++ Sources/Supporters/HTTP/HeaderFieldValueConvertible.swift
@@ -0,0 +1,12 @@
+/* *************************************************************************************************
+ HeaderFieldValueConvertible.swift
+   © 2018 YOCKOW.
+     Licensed under MIT License.
+     See "LICENSE.txt" for more information.
+ ************************************************************************************************ */
+
+/// A type that can be converted to/from an instance of `HeaderFieldValue`.
+public protocol HeaderFieldValueConvertible {
+  init?(httpHeaderFieldValue:HeaderFieldValue)
+  var httpHeaderFieldValue:HeaderFieldValue { get }
+}
