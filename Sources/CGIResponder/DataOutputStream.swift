6601710c55c004f04c0c9963c37354bdeeb163d8
YOCKOW <YOCKOW@users.noreply.github.com>
2018-11-12 15:22:18 +0900

    Reimplement `DataOutputStream`

---
+++ Sources/CGIResponder/DataOutputStream.swift
@@ -0,0 +1,22 @@
+/* *************************************************************************************************
+ DataOutputStream.swift
+   © 2018 YOCKOW.
+     Licensed under MIT License.
+     See "LICENSE.txt" for more information.
+ ************************************************************************************************ */
+ 
+import Foundation
+
+/// Similar with `TextOutputStream`. The only difference is what is given to the stream.
+public protocol DataOutputStream {
+  mutating func write(_: Data)
+}
+
+/// `FileHandle` already has `write(_:Data)`
+extension FileHandle: DataOutputStream {}
+
+/// Let `Data` conform to `DataOutputStream`
+extension Data: DataOutputStream {
+  public mutating func write(_ data:Data) { self.append(data) }
+}
+
