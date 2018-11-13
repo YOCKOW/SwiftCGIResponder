d501fad743851b73e17219acf699cc4c346d3d3e
YOCKOW <YOCKOW@users.noreply.github.com>
2018-11-13 09:29:06 +0900

    Reimplement `CGIContentOutputStream`

---
+++ Sources/CGIResponder/CGIContentOutputStream.swift
@@ -0,0 +1,48 @@
+/* *************************************************************************************************
+ CGIContentOutputStream.swift
+   © 2017-2018 YOCKOW.
+     Licensed under MIT License.
+     See "LICENSE.txt" for more information.
+ ************************************************************************************************ */
+ 
+import Foundation
+
+/// To what `CGIContent` can be given.
+/// Inherits from `DataOutputStream`.
+public protocol CGIContentOutputStream: DataOutputStream {
+  mutating func write(_:CGIContent) throws
+}
+
+extension CGIContentOutputStream {
+  private mutating func _write(contentAtPath path:String) throws {
+    guard let fh = FileHandle(forReadingAtPath:path) else {
+      throw CGIResponderError.illegalOperation
+    }
+    fh.write(to:&self)
+  }
+  
+  /// Default implementation.
+  /// Write the content represented by `content` to the receiver.
+  public mutating func write(_ content:CGIContent) throws {
+    switch content {
+    case .data(let data):
+      self.write(data)
+    case .fileHandle(let fh):
+      fh.write(to:&self)
+    case .path(let path): 
+      try self._write(contentAtPath:path)
+    case .string(let string, let encoding):
+      guard let data = string.data(using:encoding) else {
+        throw CGIResponderError.unexpectedError(message:"No data available: \(string)")
+      }
+      self.write(data)
+    case .temporaryFile(let temporaryFile):
+      temporaryFile.write(to:&self)
+    case .url(let url):
+      if url.isFileURL { try self._write(contentAtPath:url.path) }
+      else { self.write(try Data(contentsOf:url)) }
+    case .lazy(let closure):
+      try self.write(closure())
+    }
+  }
+}
