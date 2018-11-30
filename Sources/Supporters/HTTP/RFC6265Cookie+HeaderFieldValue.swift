9cbc43d90b961dbd1cadbf079f3ec1de847408a2
YOCKOW <YOCKOW@users.noreply.github.com>
2018-11-30 11:03:09 +0900

    [HTTP][Cookie] Change method about requests

--- Sources/Supporters/HTTP/RFC6265Cookie+HeaderFieldValue.swift
+++ Sources/Supporters/HTTP/RFC6265Cookie+HeaderFieldValue.swift
@@ -9,58 +9,39 @@ import BonaFideCharacterSet
 import Foundation
 import Network
 
+// Request
 extension RFC6265Cookie {
-  /// returns "name=value".
-  /// Name and Value will be encoded when `addingPercentEncoding` is true.
-  fileprivate func _nameAndValue(addingPercentEncoding: Bool = true) -> String? {
-    if addingPercentEncoding {
-      guard let name = self.name.addingPercentEncoding(withAllowedUnicodeScalars:.httpTokenAllowed)
-        else { return nil }
-      guard let value = self.value.addingPercentEncoding(withAllowedUnicodeScalars:.cookieValueAllowed)
-        else { return nil }
-      return "\(name)=\(value)"
-    } else {
-      return "\(self.name)=\(self.value)"
-    }
-  }
-}
-
-// Request Header
-extension RFC6265Cookie {
-  /// Create an instance of `HeaderFieldValue` for request header from a single cookie.
-  /// Returns `nil` if `self` should not be sent to `url`.
-  public func requestHeaderFieldValue(for url:URL,
-                                      addingPercentEncoding:Bool = true) -> HeaderFieldValue?
-  {
+  /// Returns a Boolean value that indicates whether the cookie can be sent to `url` or not.
+  public func canBeSent(to url:URL) -> Bool {
+    // Expiration
     if let expires = self.expiresDate {
-      guard expires.timeIntervalSinceNow > 0 else { return nil }
+      guard expires.timeIntervalSinceNow > 0 else { return false }
     }
     
-    guard let scheme = url.scheme?.lowercased() else { return nil }
+    // Secure
+    guard let scheme = url.scheme?.lowercased() else { return false }
     if self.isSecure {
-      guard scheme == "https" || scheme == "shttp" else { return nil }
+      guard scheme == "https" || scheme == "shttp" else { return false }
     }
     
-    guard let urlHost = url.hostComponent else { return nil }
+    // Host
+    guard let urlHost = url.hostComponent else { return false }
     let cookieHost = URL.Host(string:self.domain)
     if self.isHostOnly {
-      guard urlHost == cookieHost else { return nil }
+      guard urlHost == cookieHost else { return false }
     } else {
-      guard urlHost.domainMatches(cookieHost) else { return nil }
+      guard urlHost.domainMatches(cookieHost) else { return false }
     }
     
+    // Path
     let path = url.path.isEmpty ? "/" : url.path
     guard ({ (requestPath:String, cookiePath:String) -> Bool in
       if requestPath == cookiePath { return true }
       if requestPath.hasPrefix(cookiePath) && cookiePath.hasSuffix("/") { return true }
       if requestPath.hasPrefix("\(cookiePath)/") { return true }
       return false
-    })(path, self.path) else { return nil }
+    })(path, self.path) else { return false }
     
-    guard let string = self._nameAndValue(addingPercentEncoding:addingPercentEncoding) else {
-      return nil
-    }
-    
-    return HeaderFieldValue(rawValue:string)
+    return true
   }
 }
