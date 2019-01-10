7fdcdb78716b9ed00deec70f8fd3688a83093c88
YOCKOW <YOCKOW@users.noreply.github.com>
2019-01-10 12:19:56 +0900

    [XHTML] Define `Node` as a protocol.

---
+++ Sources/Supporters/XHTML/Node.swift
@@ -0,0 +1,10 @@
+/* *************************************************************************************************
+ Node.swift
+   © 2019 YOCKOW.
+     Licensed under MIT License.
+     See "LICENSE.txt" for more information.
+ ************************************************************************************************ */
+
+/// The nodes in the abstract, logical tree structure
+/// that represents an XHTML document like `XMLNode`.
+public protocol Node: class {}
