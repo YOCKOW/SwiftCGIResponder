becd8dca500d4ef270a1818096c2a11a9fab8b8f
YOCKOW <YOCKOW@users.noreply.github.com>
2019-01-11 16:14:20 +0900

    [XHTML] Implement `Text`.

---
+++ Sources/Supporters/XHTML/Text.swift
@@ -0,0 +1,18 @@
+/* *************************************************************************************************
+ Text.swift
+   © 2019 YOCKOW.
+     Licensed under MIT License.
+     See "LICENSE.txt" for more information.
+ ************************************************************************************************ */
+
+/// Represents text.
+open class Text: DescendantNode {
+  open var text: String
+  public init(_ text:String) {
+    self.text = text
+  }
+  
+  public var xhtmlString: String {
+    return self.text._addingAmpersandEncoding()
+  }
+}
