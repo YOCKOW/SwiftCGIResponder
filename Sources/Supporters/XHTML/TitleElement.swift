239343d56b2aa6e380a3c0b95b7ed0fbb768c703
YOCKOW <YOCKOW@users.noreply.github.com>
2019-01-14 12:56:03 +0900

    [XHTML] Implement `TitleElement`.

---
+++ Sources/Supporters/XHTML/TitleElement.swift
@@ -0,0 +1,11 @@
+/* *************************************************************************************************
+ TitleElement.swift
+   © 2019 YOCKOW.
+     Licensed under MIT License.
+     See "LICENSE.txt" for more information.
+ ************************************************************************************************ */
+
+/// /// Represents "\<title\>...\</title\>"
+open class TitleElement: SpecifiedElement {
+  internal override static var _localName: NoncolonizedName { return "title" }
+}
