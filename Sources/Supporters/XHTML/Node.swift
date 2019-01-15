f1036d7762e024f27aa42674f023d996cd97a2fc
YOCKOW <YOCKOW@users.noreply.github.com>
2019-01-15 13:02:24 +0900

    [XHTML] Add some static functions to `Node` to create elements.

--- Sources/Supporters/XHTML/Node.swift
+++ Sources/Supporters/XHTML/Node.swift
@@ -16,11 +16,27 @@ open class Node {
 }
 
 extension Node {
+  /// Create an \<body\> element.
+  public static func body(name:QualifiedName = "body",
+                          attributes:Attributes? = nil,
+                          children:[Node] = []) -> Node
+  {
+    return BodyElement(name:name, attributes:attributes, children:children)
+  }
+  
   /// Create an XHTML comment node.
   /// Fatal error will occur if `text` is invalid for XML comment.
   public static func comment(_ text:String) -> Node {
     guard let comment = Comment(text) else { fatalError("Invalid text for comment.") }
     return comment
+  }
+  
+  /// Create an \<head\> element.
+  public static func head(name:QualifiedName = "head",
+                          attributes:Attributes? = nil,
+                          children:[Node] = []) -> Node
+  {
+    return HeadElement(name:name, attributes:attributes, children:children)
   }
   
   /// Create an \<html\> element.
@@ -34,5 +50,13 @@ extension Node {
   /// Create an XHTML text node.
   public static func text(_ text:String) -> Node {
     return Text(text)
+  }
+  
+  /// Create an \<title\> element.
+  public static func title(name:QualifiedName = "title",
+                           attributes:Attributes? = nil,
+                           children:[Node] = []) -> Node
+  {
+    return TitleElement(name:name, attributes:attributes, children:children)
   }
 }
