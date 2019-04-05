032116bb54a9f1fff8e9e95c90921b3cfe1a5487
YOCKOW <YOCKOW@users.noreply.github.com>
2019-04-05 16:36:46 +0900

    Update LinuxMain.swift and related files.

--- Tests/LinuxMain.swift
+++ Tests/LinuxMain.swift
@@ -1,14 +1,10 @@
 import XCTest
 
-import HTTPTests
-import XHTMLTests
-import LibExtenderTests
 import CGIResponderTests
+import XHTMLTests
 
 var tests = [XCTestCaseEntry]()
-tests += HTTPTests.__allTests()
-tests += XHTMLTests.__allTests()
-tests += LibExtenderTests.__allTests()
 tests += CGIResponderTests.__allTests()
+tests += XHTMLTests.__allTests()
 
 XCTMain(tests)
