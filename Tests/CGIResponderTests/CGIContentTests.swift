1c71398a74744bb80d43786c8a2ae2b28d334839
YOCKOW <YOCKOW@users.noreply.github.com>
2019-05-16 12:29:29 +0900

    Move implementation of `_defaultContentType` from `CGIResponder` to `CGIContent`.

---
+++ Tests/CGIResponderTests/CGIContentTests.swift
@@ -0,0 +1,25 @@
+/* *************************************************************************************************
+ CGIContentTests.swift
+   © 2019 YOCKOW.
+     Licensed under MIT License.
+     See "LICENSE.txt" for more information.
+ ************************************************************************************************ */
+ 
+import XCTest
+@testable import CGIResponder
+
+final class CGIContentTests: XCTestCase {
+  func test_defaultContentType() {
+    var content: CGIContent = .path("/my.image.png")
+    XCTAssertEqual(content._defaultContentType, ContentType("image/png"))
+    
+    content = .string("CONTENT", encoding: .japaneseEUC)
+    XCTAssertEqual(content._defaultContentType, ContentType("text/plain; charset=euc-jp"))
+    
+    content = .url(URL(fileURLWithPath: "/my.style.css"))
+    XCTAssertEqual(content._defaultContentType, ContentType("text/css"))
+    
+    content = .xhtml(XHTMLDocument(rootElement: .init(name: "html")))
+    XCTAssertEqual(content._defaultContentType, ContentType("application/xhtml+xml; charset=utf-8"))
+  }
+}
