098f6feb227e08ed44cab523d0e24f95922cfbe3
YOCKOW <YOCKOW@users.noreply.github.com>
2020-08-03 17:42:51 +0900

    Use newer `ySwiftExtensions`.

--- Package.swift
+++ Package.swift
@@ -22,7 +22,7 @@ let package = Package(
     .package(url:"https://github.com/YOCKOW/SwiftTemporaryFile.git", from: "4.0.1"),
     .package(url:"https://github.com/YOCKOW/SwiftTimeSpecification.git", from: "3.3.0"),
     .package(url:"https://github.com/YOCKOW/SwiftXHTML.git", from: "2.0.0"),
-    .package(url:"https://github.com/YOCKOW/ySwiftExtensions.git", from: "1.4.1"),
+    .package(url:"https://github.com/YOCKOW/ySwiftExtensions.git", from: "1.5.1"),
   ],
   targets: [
     // Targets are the basic building blocks of a package. A target can define a module or a test suite.
